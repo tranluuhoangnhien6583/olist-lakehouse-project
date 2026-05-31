271a2ccf676101c8b87cc181ae644568dc4cf7e1
tranluuhoangnhien6583 <tranluuhoangnhien6583@gmail.com>
2026-05-31 08:58:22 +0700

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -1,29 +1,38 @@
 import os
 import duckdb
 import pandas as pd
-from datasets import load_dataset
 
 def main():
     md_token = os.environ.get("MOTHERDUCK_TOKEN", "").strip()
-    hf_token = os.environ.get("HF_TOKEN", "").strip()
-
     if not md_token:
         raise ValueError("Không tìm thấy MOTHERDUCK_TOKEN!")
 
     print("Đang kết nối tới MotherDuck...")
     con = duckdb.connect(f'md:olist_lakehouse?motherduck_token={md_token}')
 
-    print("Đang tải dữ liệu Olist E-Commerce từ Hugging Face...")
-    dataset = load_dataset("aviahYadler/Olist_Ecommerce_Dataset", split="train", token=hf_token)
-    
-    df = dataset.to_pandas()
-    print(f"Tải thành công {len(df)} dòng dữ liệu.")
+    base_url = "https://huggingface.co/datasets/aviahYadler/Olist_Ecommerce_Dataset/resolve/main/"
+    tables = {
+        "raw_orders": "olist_orders_dataset.csv",
+        "raw_order_items": "olist_order_items_dataset.csv",
+        "raw_order_reviews": "olist_order_reviews_dataset.csv",
+        "raw_customers": "olist_customers_dataset.csv",
+        "raw_order_payments": "olist_order_payments_dataset.csv",
+        "raw_geolocation": "olist_geolocation_dataset.csv",
+        "raw_products": "olist_products_dataset.csv",
+        "raw_sellers": "olist_sellers_dataset.csv",
+        "raw_category_translation": "product_category_name_translation.csv"
+    }
 
-    # Nạp dữ liệu vào MotherDuck (Tầng Bronze)
-    print("Đang nạp dữ liệu thô vào bảng olist_raw...")
-    con.execute("CREATE OR REPLACE TABLE olist_raw AS SELECT * FROM df")
+    for table_name, file_name in tables.items():
+        print(f"Đang tải dữ liệu bảng: {table_name} ({file_name})...")
+        file_url = base_url + file_name
+ 
+        df = pd.read_csv(file_url)
+        print(f"-> Tải thành công {len(df)} dòng. Đang lưu vào Lakehouse...")
+        
+        con.execute(f"CREATE OR REPLACE TABLE {table_name} AS SELECT * FROM df")
 
-    print("Hoàn tất nạp dữ liệu!")
+    print("Hoàn tất nạp toàn bộ 9 bảng dữ liệu Olist!")
     con.close()
 
 if __name__ == "__main__":
